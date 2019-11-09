704a60a05666a770de7c2b5446885614cdc6708b
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2019-11-08 20:33:52 -0600

    intialized file system. setup api keys in .env file. keys imported into liri.js file successfully

---
+++ liri.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const spotifyApiKey = process.env.spotify_api_key;
+const omdbApiKey = process.env.omdb_api_key;
+const bandsInTownApiKey = process.env.bands_in_town_api_key;
+
+console.log(spotifyApiKey);
+console.log(omdbApiKey);
+console.log(bandsInTownApiKey);
+
+
